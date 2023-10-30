40d7bef0d9295cce3c68f969ac81db363f9072fb
Evgenii Zakirov <e.zakirov81@outlook.com>
2023-10-30 11:08:12 -0600

    first commit

---
+++ app/components/ListItem.tsx
@@ -0,0 +1,23 @@
+import getFormattedDate from '@/lib/getFormattedDate'
+import Link from 'next/link'
+
+type Props = {
+    post: Meta
+}
+
+export default function ListItem({post}: Props) {
+    const {id, title, date} = post
+    const formattedDate = getFormattedDate(date)
+  return (
+    <li className='mt-4 text-xl dark:text-white/90'>
+      <Link 
+        href={`/posts/${id}`}
+        className='underline hover:text-black/70 dark:hover:text-white/50'
+      >
+        {title}
+      </Link>
+      <br />
+      <p className='text-sm mt-1'>{formattedDate}</p>
+    </li>
+  )
+}
